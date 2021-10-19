5ab90a4c7702247937242250a5c658d30c4cf7f8
Amit Kumar <amit311kr@gmail.com>
2021-10-20 01:11:38 +0530

    Improved design

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const session = require("express-session");
 const PORT = require("./config").port;
+const SESSION = require("./config").session;
+const errorMiddlerware = require("./middlewares/error");
+const db = require("./config/database");
+
+db.authenticate()
+  .then(() => console.log("Connection to Database established Successfully"))
+  .catch((err) => {
+    console.log("Unable to connect to Database", err.message);
+    process.exit(1)
+  });
 
 const app = express();
+//app.use(session({ secret: SESSION.secret }))
+
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const user = require("./routes/userRoutes")
 
+//Routes
 app.use('/user', user);
+
+
+//Error Middleware int the end
+app.use(errorMiddlerware);
 
 app.listen(PORT, (err) => {
   if (err) {
--- models/user.js
+++ models/user.js
@@ -1,27 +1,82 @@
 const { DataTypes } = require("sequelize");
-const { database } = require("../config");
-const { db } = require("../loaders/connectDb");
-const { hash } = require("../utils/encrypt")
-const { pwdstring } = require("../utils/pwdgenerator.js")
+const db = require("../config/database");
+const { hash } = require("../utils/encrypt");
+const errorHandler = require("../utils/errorHandler");
 
 const user = db.define("user", {
-  userId: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
-  firstName: { type: DataTypes.STRING, allowNull: false, validate: { isAlpha: true } },
-  lastName: { type: DataTypes.STRING, allowNull: false, validate: { isAlpha: true } },
-  emailId: {
-    type: DataTypes.STRING, allowNull: false, unique: true, validate: {
-      isEmail: true, isNitcEmail(value) { if (value.slice(-11) != "@nitc.ac.in") { throw new Error('Only nitc emails are allowed'); } }
-    }
+  userId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
   },
-
-  yearofStudy: { type: DataTypes.INTEGER.UNSIGNED,allowNull: false},
-  branch: { type: DataTypes.STRING, allowNull: false, validate: { isAlpha: true } },
-  password: { type: DataTypes.STRING, allowNull: false,defaultValue=pwdstring, set(value) { this.setDataValue('password', hash(this.emailId + value)); } },
-  dob: { type: DataTypes.DATEONLY, allowNull: false, validate: { isValidDob(value) { if (value > Date()) { throw new Error('Invalid Date of Birth'); } } } },
-  gender: { type: DataTypes.STRING(20), allowNull: false, set(value) { this.setDataValue('gender', value.toUpperCase()); }, validate: { isIn: [['MALE', 'FEMALE', 'DECLINE TO SAY']] } },
-  profilePic: { type: DataTypes.STRING, set(value) { if (this.gender == 'FEMALE') { this.setDataValue('profilePic', 'female.png') } else { this.setDataValue('profilePic', 'male.png') } } }
-}
-);
-module.exports = {
-  user,
-};
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue("firstName", value.toUpperCase());
+    },
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue("lastName", value.toUpperCase());
+    },
+  },
+  emailId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      isNitcEmail(value) {
+        if (value.slice(-11) != "@nitc.ac.in") {
+          throw new errorHandler("Only nitc emails are allowed", 406);
+        }
+      },
+    },
+    set(value) {
+      this.setDataValue("emailId", value.toLowerCase());
+    },
+  },
+  passingYear: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+  branch: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue("branch", value.toUpperCase());
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue("password", hash(this.emailId + value));
+    },
+  },
+  dob: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    validate: { isDate: true },
+  },
+  gender: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    set(value) {
+      this.setDataValue("gender", value.toUpperCase());
+    },
+    validate: { isIn: [["MALE", "FEMALE", "DECLINE TO SAY"]] },
+  },
+  profilePic: {
+    type: DataTypes.STRING,
+    set(value) {
+      if (this.gender == "FEMALE") {
+        this.setDataValue("profilePic", "female.png");
+      } else {
+        this.setDataValue("profilePic", "male.png");
+      }
+    },
+  },
+});
+user.sync();
+module.exports = user;
