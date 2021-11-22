32078970caf4623f9c69c50858de81e369b1ddf3
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-11-22 09:30:40 +0530

    comment input field added

--- frontend/src/components/post/post.js
+++ frontend/src/components/post/post.js
@@ -1,30 +1,34 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Button from "@material-ui/core/Button";
-import ReportIcon from '@mui/icons-material/Report';
-import CommentIcon from '@mui/icons-material/Comment';
-
+import ReportIcon from "@mui/icons-material/Report";
+import SendIcon from "@mui/icons-material/Send";
+import CommentIcon from "@mui/icons-material/Comment";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
@@ -40,110 +44,136 @@ export default function Post() {
 
   return (
     <center>
-    <Card sx={{ maxWidth: 600 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings" onClick={() => {
-            if(report) {
-              alert("Post is already reported by you");
-            }
-            else{
-              alert("Post Reported");
-              setReport(true);
-            }
-          }}>
-            {
-              report ?
-              (<ReportIcon style={{ color: "red" }}/>):
-              (<ReportIcon style={{ color: "grey" }}/>)
-            }
-          </IconButton>
-        }
-        title="post title"
-        subheader="post creator name"
-      />
-      <CardMedia 
-        component="img"
-        height="auto"
-        image="https://cdn.pixabay.com/photo/2014/11/13/06/12/boy-529067__340.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          content text
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={() => {
-          if(liked){
-            alert("already liked");
+      <Card sx={{ maxWidth: 600 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
           }
-          else{
-            alert("liked");
-            setLikes(likes + 1);
-            setLiked(!liked);
+          action={
+            <IconButton
+              aria-label="settings"
+              onClick={() => {
+                if (report) {
+                  alert("Post is already reported by you");
+                } else {
+                  alert("Post Reported");
+                  setReport(true);
+                }
+              }}
+            >
+              {report ? (
+                <ReportIcon style={{ color: "red" }} />
+              ) : (
+                <ReportIcon style={{ color: "grey" }} />
+              )}
+            </IconButton>
           }
-          //alert("clicked");
-        }}>
-          {liked ? <FavoriteIcon style={{ color: "red" }}/> : <FavoriteIcon style={{ color: "grey" }}/>}
-          <p>{likes}&emsp;</p>
-        </IconButton>
-        <IconButton aria-label="share" onClick={() => {
-         const el = document.createElement('input');
-         el.value = window.location.href;
-         document.body.appendChild(el);
-         el.select();
-         document.execCommand('copy');
-         document.body.removeChild(el);
-         alert("post link copied ");
-        }}>
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <p>comments &emsp;</p>
-          <CommentIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+          title="post title"
+          subheader="post creator name"
+        />
+        <CardMedia
+          component="img"
+          height="auto"
+          image="https://cdn.pixabay.com/photo/2014/11/13/06/12/boy-529067__340.jpg"
+          alt="Paella dish"
+        />
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          <Typography variant="body2" color="text.secondary">
+            content text
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions disableSpacing>
+          <IconButton
+            aria-label="add to favorites"
+            onClick={() => {
+              if (liked) {
+                alert("already liked");
+              } else {
+                alert("liked");
+                setLikes(likes + 1);
+                setLiked(!liked);
+              }
+              //alert("clicked");
+            }}
+          >
+            {liked ? (
+              <FavoriteIcon style={{ color: "red" }} />
+            ) : (
+              <FavoriteIcon style={{ color: "grey" }} />
+            )}
+            <p>{likes}&emsp;</p>
+          </IconButton>
+          <IconButton
+            aria-label="share"
+            onClick={() => {
+              const el = document.createElement("input");
+              el.value = window.location.href;
+              document.body.appendChild(el);
+              el.select();
+              document.execCommand("copy");
+              document.body.removeChild(el);
+              alert("post link copied ");
+            }}
+          >
+            <ShareIcon />
+            <p>&emsp;</p>
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <p> &emsp;</p>
+            <CommentIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto">
+          <CardContent>
+            <Typography paragraph></Typography>
+            <FormControl fullWidth sx={{ m: 1 }}>
+              <InputLabel htmlFor="outlined-adornment-amount">
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-amount"
+                // value={values.amount}
+                onChange={() => {
+                  //console.log("changed");
+                }}
+                label="Comment"
+              />
+              <Button variant="contained" endIcon={<SendIcon />}>
+                Add Comment
+              </Button>
+            </FormControl>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that don’t
+              open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     </center>
   );
 }
