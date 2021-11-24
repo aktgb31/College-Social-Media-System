791fa0b2394306ccaeee8d0a6cc116de3027a9f2
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-11-24 22:13:32 +0530

    post form created

--- frontend/src/components/createpost/createpost.js
+++ frontend/src/components/createpost/createpost.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import Button from "@mui/material/Button";
+// import UploadButtons from "../imguploader/imguploader";
+import { Grid, Paper, TextField } from "@mui/material";
+import Navbar  from "../navbar/navbar";
 function CreatePost() {
-    return (
-        <div>
-            crete a post here
-        </div>
-    )
+  return (
+    <>
+    
+    <Navbar/>
+      <h1>Create Your Post Here</h1>
+        <form class="form">
+        <input
+        type="text"
+        name="content"
+        // value={user.lastName}
+        placeholder="Type your content here"
+        // onChange={handleChange}
+      ></input>
+              <input
+        type="file"
+        name="lastName"
+        // value={user.lastName}
+        placeholder="Your Last Name"
+        // onChange={handleChange}
+      ></input>
+      <div className="button">
+        Post
+      </div>
+        </form>
+    </>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
