2ba0bfd5ecf7fc0608c580232fbc56ab704d1d06
Amit Kumar <amit311kr@gmail.com>
2021-11-28 22:25:52 +0530

    fixed auth bug

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,12 +4,12 @@ const catchAsyncErrors = require("./catchAsyncErrors");
 // To stop already logined user from accessing login and register page
 exports.isLoginedUser = catchAsyncErrors(async(req, res, next) => {
     if (req.session && req.session.userId)
-        next(new ErrorHandler(403, "User already logged in"));
-    else next();
+        return next(new ErrorHandler(403, "User already logged in"));
+    else return next();
 });
 
 // To stop not logined user from accessing other features
 exports.isAuthenticatedUser = catchAsyncErrors(async(req, res, next) => {
-    if (req.session && req.session.userId) next();
-    else next(new ErrorHandler(401, "Please login to access this resource"));
+    if (req.session && req.session.userId) return next();
+    else return next(new ErrorHandler(401, "Please login to access this resource"));
 });
