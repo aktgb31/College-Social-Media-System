10edd1165777e016947b82043f7a3d9dad00af3d
Amit Kumar <amit311kr@gmail.com>
2021-11-30 17:24:25 +0530

    fixed descirption bug

--- backend/utils/eventUtils.js
+++ backend/utils/eventUtils.js
@@ -1,7 +1,7 @@
 const ErrorHandler = require("./errorHandler");
 const validator = require('validator');
 
-exports.formatEventDetails = (name, description, time) => {
+exports.formatEventDetails = (name, time) => {
     if (name == null || name == "")
         throw new ErrorHandler(400, "Event Name cannot be empty");
     if (!validator.isISO8601(time))
