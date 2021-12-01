1fcbf6772a9361b4cb88dc11e1a8300442333e19
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-12-02 00:35:31 +0530

    chat fetched done

--- frontend/src/components/people/people.js
+++ frontend/src/components/people/people.js
@@ -5,18 +5,22 @@ import { Link } from "react-router-dom";
 import "./people.css";
 function PeopleComponent(props) {
   return (
-    
-    
     <div id="outer-message">
       <Card id="chat-card">
         <Card.Header as="h5">{props.author}</Card.Header>
         <Card.Body>
-          <Link to="/message">
-          <Button variant="primary" id="btn-chat">Chat</Button>
+          <Link
+            to={{
+              pathname: "/message",
+              search: `?senderId=${props.id_}`,
+            }}
+          >
+            <Button variant="primary" id="btn-chat">
+              Chat
+            </Button>
           </Link>
         </Card.Body>
       </Card>
-      
     </div>
   );
 }
