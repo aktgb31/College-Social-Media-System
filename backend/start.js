c8e7b9dec5b1f5b0feb66a5f0f7c63243a13fa95
Amit Kumar <amit311kr@gmail.com>
2021-12-03 00:59:26 +0530

    added delay for server start

--- backend/start.js
+++ backend/start.js
@@ -1,14 +1,20 @@
 const { spawnSync } = require("child_process");
 
-function runServer() {
 
+function runServer() {
     let child = spawnSync("node", ["server.js"], { shell: false, stdio: 'inherit' });
     console.log(`Server stopped with code ${child.status}`);
 }
 
-let i = 1;
-while (true) {
-    console.log(`Starting server for ${i} time`);
-    i = i + 1;
-    runServer();
-}
+
+async function start() {
+    let i = 1;
+    while (true) {
+        console.log(`Starting server for ${i} time`);
+        i = i + 1;
+        runServer();
+        await new Promise(r => setTimeout(r, 1000));
+    }
+};
+
+start();
