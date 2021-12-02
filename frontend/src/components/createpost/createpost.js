010846fb207d755018790174aff45e4625cdce26
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-12-02 12:24:47 +0530

    create post added

--- frontend/src/components/createpost/createpost.js
+++ frontend/src/components/createpost/createpost.js
@@ -2,41 +2,68 @@ import React from "react";
 import Button from "@mui/material/Button";
 // import UploadButtons from "../imguploader/imguploader";
 import { Grid, Paper, TextField } from "@mui/material";
-import Navbar  from "../navbar/navbar";
+import Navbar from "../navbar/navbar";
 import "./createpost.css";
-
+import { useState } from "react";
 function CreatePost() {
-  
+  const [user, setUser] = useState({ content: "" });
+  const updateDetails = () => {
+    //console.log(user);
+    fetch("/api/post/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        alert("Profile updated");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error in Updating Profile");
+      });
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    //console.log(user);
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
   return (
     <>
-    
-    <Navbar/>
-      
-       <div>&nbsp;&nbsp;</div>
-       <div>&nbsp;&nbsp;</div>
-       <div>&nbsp;&nbsp;</div>
-       <div>&nbsp;&nbsp;</div>
-        <form class="form">
-           <h1>Create Your Post Here</h1> 
-         
+      <Navbar />
+
+      <div>&nbsp;&nbsp;</div>
+      <div>&nbsp;&nbsp;</div>
+      <div>&nbsp;&nbsp;</div>
+      <div>&nbsp;&nbsp;</div>
+      <form class="form">
+        <h1>Create Your Post Here</h1>
+
         <input
-        type="text"
-        name="content" id="content-text"
-        // value={user.lastName}
-        placeholder="         Type your content here"
-        // onChange={handleChange}
-      ></input>
-              <input
-        type="file"
-        name="lastName"
-        // value={user.lastName}
-        placeholder="Your Last Name"
-        // onChange={handleChange}
-      ></input>
-      <div className="button">
-        Post
-      </div>
-        </form>
+          type="text"
+          name="content"
+          id="content-text"
+          // value={user.lastName}
+          placeholder="         Type your content here"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="file"
+          name="lastName"
+          // value={user.lastName}
+          placeholder="Your Last Name"
+          // onChange={handleChange}
+        ></input>
+        <Button id="update-button" variant="outlined" onClick={updateDetails}>
+          Create Post
+        </Button>
+      </form>
     </>
   );
 }
