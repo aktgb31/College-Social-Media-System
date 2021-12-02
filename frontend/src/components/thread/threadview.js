a7c1ef06864aafb76bf0a8934ffb22250726110f
GC6610 <gopal_b190501cs@nitc.ac.in>
2021-12-02 20:24:07 +0530

    threads page done

--- frontend/src/components/thread/threadview.js
+++ frontend/src/components/thread/threadview.js
@@ -10,46 +10,141 @@ import InputLabel from "@mui/material/InputLabel";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "react-bootstrap/Button";
 import "./thread.css";
+import Hppost from "../hppost/hppost";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios"
+import { useHistory } from "react-router-dom"
 
 function ThreadView() {
-  const [user, setUser] = useState({
-    message: "",
-  });
+  const [post,setPost] = useState([]);
+  const [user,setUser] = useState(null);
   const search = useLocation().search;
-  const senderId = new URLSearchParams(search).get("threadId");
-  console.log(senderId);
+  const currthreadId = new URLSearchParams(search).get("threadId");
+  console.log(currthreadId);
+  useEffect(async () => {
+    const response= await fetch("/api/post");
+    const data= await response.json();
+    setPost(data.data);
+    const res= await fetch("/api/user/profile/me");
+    const dat= await res.json();
+    try {
+       const tr=dat.data.student.firstName;
+       setUser(tr);
+    }
+  catch(err) {
+    const tr=dat.data.club.name;
+    setUser(tr);
+  }
+  // const id = dat.data.userId;
+    
+  //   setId(id)
+  //   console.log({id});
+    const pstresponse = await axios.get('/api/post', {
+      params: {
+        threadId: currthreadId
+      }, 
+      withCredentials: true
+    
+    })
+    
+    // const data = await response.json();
+    // console.log(data);
+    setPost(pstresponse.data.data);
+    console.log(pstresponse)
+    
+    
+  },[])
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [content, setContent] = useState({ content: "" });
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+  const history = useHistory()
+  const updateDetails = () => {
+    //console.log(user);
+    const formData = new FormData();
+    console.log(content.content);
+    formData.append('relatedImage', selectedFile);
+    formData.append('content', content.content);
+    formData.append('threadId',currthreadId);
+    fetch("/api/post/", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        alert("Message sent in thread");
+        history.push("/thread");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error in Posting");
+      });
+  };
   const handleChange = (e) => {
     const { name, value } = e.target;
     //console.log(user);
-    setUser({
-      ...user,
+    setContent({
+      ...content,
       [name]: value,
     });
   };
-  const sendMessage = () => {
-    console.log(user);
-    alert("Message sent");
-  };
+  
   return (
-    <div>
-      <NavbarComponent />
-      <h1 id="thread-name">Thread Name</h1>
-      <ThreadMessage author="Utkarsh" content="CP is god" />
-      <ThreadMessage author="Kunal" content="CP is shit" />
-      <form id="input-msg-form">
+    <>
+     <NavbarComponent name={user}/>
+        <center><h1>Thread ID : {currthreadId}</h1></center>
+        <div>&nbsp;&nbsp;</div>
+      <div>&nbsp;&nbsp;</div>
+      <form class="form">
+        <h4>Enter your Thread Message here</h4>
+
         <input
           type="text"
-          placeholder="Type a message"
-          id="thread-msg"
-          name="message"
-          value={user.message}
+          name="content"
+          id="content-thread-text"
+          value={content.content}
+          placeholder="Type your content here"
           onChange={handleChange}
+        ></input>
+        <input
+          type="file"
+          name="file"
+          // value={user.lastName}
+          onChange={changeHandler}
         />
-        <Button variant="primary" onClick={sendMessage} id="send-msg-btn">
-          Send
+        {isFilePicked ? (
+          <div>
+            <p>Filename: {selectedFile.name}</p>
+            <p>Filetype: {selectedFile.type}</p>
+            <p>Size in bytes: {selectedFile.size}</p>
+            <p>
+              lastModifiedDate:{' '}
+              {selectedFile.lastModifiedDate.toLocaleDateString()}
+            </p>
+          </div>
+        ) : (
+          <p>Select a file to show details</p>
+        )}
+        <Button id="update-button" variant="outlined" onClick={updateDetails}>
+          Send Message
         </Button>
       </form>
-    </div>
+        <div>&nbsp;&nbsp;</div>
+      <div>&nbsp;&nbsp;</div>
+        {post.map( (postdetails)=>{
+            return<Hppost title="home" id_={postdetails.postId} author={postdetails.creatorId} content={postdetails.content} postId={postdetails.postId}/>
+        
+        })}
+      {/* <Hppost title="Title fetched" author="Navnit Anand" content="this is the content"/>
+      <Hppost title="Title fetched" author="Amit Kumar" content="this is the content"/>
+      <Hppost title="Title fetched" author="Gopal Chaudhary" content="this is the content"/>
+      <Hppost title="Title fetched" author="Navnit Anand" content="this is the content"/> */}
+    </>
   );
 }
 
