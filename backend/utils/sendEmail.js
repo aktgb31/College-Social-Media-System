c12d96b327cb749c7df6e50987f2f1113a396b76
Amit Kumar <amit311kr@gmail.com>
2021-12-03 17:41:13 +0530

    changed email content

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,7 @@ const { EMAIL, ENVIRONMENT } = require("../config");
 const verificationSubject = "College Social Media System Account Verification";
 
 function verificationHTML(to, password) {
-    return `<p>Your account on College Social Media System has been created sucessfully. Use following details to sign in.<br>User_ID : ${to}<br>Password : ${password}</p>`;
+    return `<p>Your account on College Social Media System has been created sucessfully. Use following details to sign in. After logging in, you are advised to change password as per your wishes to make the account more secure.<br>User_ID : ${to}<br>Password : ${password}</p>`;
 }
 
 const transporter = nodemailer.createTransport({
