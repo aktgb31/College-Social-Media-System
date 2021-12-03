ae27d2843367083043cbfced061dad0c89addf36
GC6610 <gopal_b190501cs@nitc.ac.in>
2021-12-03 20:45:28 +0530

    search page done

--- frontend/src/components/navbar/search.js
+++ frontend/src/components/navbar/search.js
@@ -6,13 +6,16 @@ import NavbarComponent from "../navbar/navbar";
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import axios from "axios"
+import PeopleComponent from "../people/people";
+import { useHistory } from "react-router-dom";
 
 function Searchprofile() {
     
-    
-
+    const history = useHistory();
+    const [post,setPost] = useState([]);
     const [user, setUser] = useState({
-        name: "",
+        uname: "",
         
     })
 
@@ -23,16 +26,20 @@ function Searchprofile() {
             [name]: value
         })
     }
-    // const register = () => {
-    //     const response = await axios.get('/api/search', {
-    //   params: {
-    //     name: {name}
-    //   }, 
-    //   withCredentials: true
+    const search = async () => {
+      
+      const response = await axios.get('/api/user/search', {
+      params: {
+        keyword: user.uname
+      }, 
+      withCredentials: true
     
-    // })
+    })
+    setPost(response.data.data);
+    console.log(response)
+    history.push('/search');
         
-    // }
+    }
 
     
     const [uname, setUname] = useState();
@@ -58,18 +65,34 @@ function Searchprofile() {
             <div id="search-box">
             <div class="input-group" id="search-outer">
             <div id="input-outer">
-              <input type="text" name="name" value={user.name} placeholder="search User by name" onChange={handleChange} id="form1" class="form-control" />
-              <label class="form-label" for="form1">Search</label>
+              <input type="text" name="uname" value={user.uname} placeholder="search User by name / emailId" onChange={handleChange} id="form1" class="form-control" />
+              <label class="form-label" for="form1"  >Search</label>
             </div>
             <div>&nbsp;&nbsp;&nbsp;</div>
-<button type="button" id="btn-nav"  >
+<button type="button" id="btn-nav" onClick={search} >
               <FaSearch/>
             </button></div>
             </div>
+            <div id="chat-box">
+        <div>
+          <h2 id="chat-title">&nbsp;Profiles</h2>
+          {post && post.map((postdetails) => {
+            
+            if(!postdetails.name) {
+               return<PeopleComponent author={postdetails.firstName+" "+postdetails.lastName} id_={postdetails.userId}/>
+             }
+            else{
+               
+               return<PeopleComponent author={postdetails.name} id_={postdetails.userId}/>
+             }
+          })}
+        </div>
+      </div>
 
 
         
-        </div></>
+        </div>
+        </>
     )
 }
 
